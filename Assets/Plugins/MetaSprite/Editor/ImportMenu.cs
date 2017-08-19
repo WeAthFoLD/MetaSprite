69bed3b807e0a738d3bdcda7029eefa765861c7a
WeAthFolD <weathfold@li-dev.cn>
2017-08-19 12:58:04 +0800

    Init project

---
+++ Assets/Plugins/MetaSprite/Editor/ImportMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+using System.Linq;
+
+namespace ImportMenu {
+
+public static class ImportMenu {
+
+    [MenuItem("Assets/Aseprite/Import", priority = 60)]
+    static void MenuClicked() {
+
+    }
+
+    [MenuItem("Assets/Aseprite/Import", true)]
+    static bool ValidateMenu() {
+        return GetSelectedAseprites().Count() > 0;
+    }
+
+    static DefaultAsset[] GetSelectedAseprites() {
+        return Selection.GetFiltered<DefaultAsset>(SelectionMode.DeepAssets)
+                        .Where(it => {
+                            var path = AssetDatabase.GetAssetPath(it);
+                            return path.EndsWith(".ase") || path.EndsWith(".aseprite");
+                        })
+                        .ToArray();
+    }
+
+}
+
+}
